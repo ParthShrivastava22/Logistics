8b587f76fc73b87ea7f8e33b6139e23ede8144b7
Parth Shrivastava <shrivastavaparth03@gmail.com>
2025-03-27 20:49:12 +0530

    MongoDB Server Storage

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,17 +1,17 @@
-// Load models FIRST
-require("../models/user.model"); // Import User model
-require("../models/driver.model"); // Import Driver model if needed
-require("../models/delivery.model");
-
+// db.js (demo mode using mongodb-memory-server)
+const { MongoMemoryServer } = require("mongodb-memory-server");
 const mongoose = require("mongoose");
 
-function connectToDb() {
-  mongoose
-    .connect(process.env.DB_CONNECT)
-    .then(() => {
-      console.log("Connected to DB");
-    })
-    .catch((err) => console.log(err));
+async function connectToDb() {
+  if (process.env.DEMO_MODE === "true") {
+    const mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+    await mongoose.connect(uri);
+    console.log("Connected to in-memory MongoDB (demo mode)");
+  } else {
+    await mongoose.connect(process.env.DB_CONNECT);
+    console.log("Connected to DB");
+  }
 }
 
 module.exports = connectToDb;
